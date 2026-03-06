814be38fddcabb627ce66ecbb6eb2f5e2e54d882
Brian Jarrett <celttechie@gmail.com>
2026-03-06 07:15:10 -0600

    Restrict climber controls to endgame (last 30s) (#89)

--- roborio/robotcontainer.py
+++ roborio/robotcontainer.py
@@ -209,15 +209,20 @@ class RobotContainer:
 
         self.driver_xbox.y().whileTrue(self.intake.runForward())
 
-        self.driver_xbox.leftBumper().onTrue(self.climber.deploy_command())
-        self.driver_xbox.leftTrigger().whileTrue(self.climber.stow_command())
+        is_endgame = Trigger(self.shift_tracker.is_endgame)
+        self.driver_xbox.leftBumper().and_(is_endgame).onTrue(
+            self.climber.deploy_command()
+        )
+        self.driver_xbox.leftTrigger().and_(is_endgame).whileTrue(
+            self.climber.stow_command()
+        )
 
-        # POV lift controls only when deployed
+        # POV lift controls only when deployed AND in endgame
         is_deployed = Trigger(self.climber.is_deployed)
-        self.driver_xbox.povUp().and_(is_deployed).whileTrue(
+        self.driver_xbox.povUp().and_(is_deployed).and_(is_endgame).whileTrue(
             self.climber.lift_forward_cmd()
         )
-        self.driver_xbox.povDown().and_(is_deployed).whileTrue(
+        self.driver_xbox.povDown().and_(is_deployed).and_(is_endgame).whileTrue(
             self.climber.lift_reverse_cmd()
         )
 
